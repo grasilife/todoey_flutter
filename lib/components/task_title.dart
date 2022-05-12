9f8eae82a680ab906c923f85059a2211d2b02425
fuguoqiang <434543162@qq.com>
2022-05-13 00:28:56 +0800

    状态传递

--- lib/components/task_title.dart
+++ lib/components/task_title.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
 
-class TaskTitle extends StatelessWidget {
+class TaskTitle extends StatefulWidget {
   const TaskTitle({Key? key}) : super(key: key);
+
+  @override
+  State<TaskTitle> createState() => _TaskTitleState();
+}
+
+class _TaskTitleState extends State<TaskTitle> {
+  bool isChecked = true;
+  void checkboxCallback(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text("This is a task"),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      title: Text(
+        "This is a task",
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckBox(
+        checkboxState: isChecked,
+        checkboxCallback: checkboxCallback,
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkboxState;
+  final Function checkboxCallback;
+  const TaskCheckBox({
+    required this.checkboxState,
+    required this.checkboxCallback,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: (value) {
+        checkboxCallback(value);
+      },
     );
   }
 }
